30e39057650af5e937605e758de617b9908797af
RubenBaader <rubenbaader@gmail.com>
2023-03-21 13:43:31 +0100

    working on piping through http protocol

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GetProductsService } from './get-products/get-products.service';
+import { fromEvent, take } from 'rxjs';
 
 @Component({
   selector: 'app-search',
   templateUrl: './app.component.html',
+  providers: [ GetProductsService ],
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'projectSearch';
+
+  constructor(
+    private products: GetProductsService
+  ) {}
+  ngOnInit(): void {
+    this.testNonObsJson();
+    // this.testObsJson();
+  }
+
+  testObsJson() {
+    console.log("helloFromObs");
+    const a = this.products.getObservableJson().pipe(take(5)).subscribe(x => {return x.body});
+    console.log(a);
+  }
+  testNonObsJson() {
+    // const a = this.products.getJson();
+    return this.products.getJson();
+    // console.log(a + "------------------------")
+    // const b = a.pipe(take())
+    // const b = a.forEach(value => {console.log(value)})
+    // return a;
+  }
+
+  /* search bar solution from learnrxjs.com -- most likely plug and play? */
+  // observable of values from a text box, pipe chains operators together
+    /* inputValue
+    .pipe(
+      // wait for a 200ms pause
+      debounceTime(200),
+      // if the value is the same, ignore
+      distinctUntilChanged(),
+      // if an updated value comes through while request is still active cancel previous request and 'switch' to new observable
+      switchMap(searchTerm => typeaheadApi.search(searchTerm))
+    )
+    // create a subscription
+    .subscribe(results => {
+      // update the dom
+    }); */
+
 }
---
+++ src/app/get-products/get-products.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry, take } from 'rxjs/operators';
+
+import { Product, SearchProductResponse } from './products';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetProductsService {
+  productUrl = 'assets/products.json'
+
+  constructor(private http : HttpClient) { }
+
+  testInjectable() {
+    console.log("Hello from GetProductService!");
+  }
+
+  getJson() {
+    const a : Observable<Product> = this.http.get<Product>(this.productUrl)
+      .pipe(
+        take(5),
+        retry(3),
+        catchError(this.handleError)
+      )
+    console.log(a.subscribe(), "ASYNC #######################################")
+    /* return this.http.get<Product>(this.productUrl)
+      .pipe(
+        retry(3),
+        catchError(this.handleError)
+      ) */
+    return a.subscribe();
+  }
+
+  getObservableJson(): Observable<HttpResponse<Product>> {
+    return this.http.get<Product>(
+      this.productUrl, { observe: 'response' } );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
+
+}
